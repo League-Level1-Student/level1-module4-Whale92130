4320a742981792ab69e34261340002d72bb1fc53
Marcus Solt <marcussolt723@gmail.com>
2022-10-07 18:31:46 -0700

    finshed typing tutor and worked on book of illusions

--- src/_03_typing_tutor/TypingTutor.java
+++ src/_03_typing_tutor/TypingTutor.java
@@ -1,27 +1,67 @@
 package _03_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	char currentLetter;
 
 	void setup(){
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		currentLetter = generateRandomLetter();
-		JLabel label = new JLabel();
 		label.setFont(label.getFont().deriveFont(28.0f));
 		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setText(""+generateRandomLetter());
+		frame.addKeyListener(this);
+		frame.add(panel);
+		panel.add(label);
+		frame.pack();
+		
 	}
 	char generateRandomLetter() {
 	    Random r = new Random();
 	    return (char) (r.nextInt(26) + 'a');
 	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+			System.out.println("you typed: " + e.getKeyChar());
+		if (e.getKeyChar() == (currentLetter)) {
+			System.out.println("Correct");
+			System.out.println("Correct");
+			System.out.println("Correct");
+			panel.setBackground(Color.GREEN);
+		}
+		else {
+			panel.setBackground(Color.RED);
+		}
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(""+currentLetter);
+		
+	}
+	
+
+	
+	
 }
--- src/_04_book_of_illusions/BookOfIllusions.java
+++ src/_04_book_of_illusions/BookOfIllusions.java
@@ -8,9 +8,11 @@ package _04_book_of_illusions;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,24 +20,50 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		String image;
+		String image2;
+		String imageQ;
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
 		// 4. find 2 images and save them to your project’s _06_book_of_illusions folder
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		 image = "pic1.jpeg";
+		 image2 = "pic2.jpeg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label.add(loadImageFromComputer(image));
 		// 8. add your JLabel to the frame
+		frame.add(label);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked");
 		// 12. remove everything from the frame that was added earlier
+		frame.removeAll();
 		// 13. load a new image like before (this is more than one line of code)
+		 Random r = new Random();
+		 int ra = r.nextInt(2);
+		if (ra == 1) {
+			 imageQ = image;
+		}
+		else {
+			imageQ = image2;
+		}
+		
+		label.add(loadImageFromComputer(imageQ));
+		frame.add(label);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
